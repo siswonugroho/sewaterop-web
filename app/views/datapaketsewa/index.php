60390d563dc255e3d389868eb220c0a2342ed3d0
Sovana Siswonugroho <vana.hantoro7@gmail.com>
2021-01-10 20:59:34 +0700

    Fitur keseluruhan paket sewa

---
+++ app/views/datapaketsewa/index.php
@@ -0,0 +1,101 @@
+<section class="col-md mt-3">
+    <header class="p-2 bg-white">
+        <h2 class="text-center">Daftar Paket Sewa</h2>
+    </header>
+    <main id="list-paket">
+        <?php Flasher::showFlash(); ?>
+        <span class="d-flex py-3 mx-auto sticky-top bg-white">
+            <a class="btn btn-primary mr-3 d-none d-md-flex align-items-center" href="<?= BASEURL ?>/datapaketsewa/pagetambah">
+                <svg class="bi mr-2" width="24" height="24" fill="currentColor">
+                    <use xlink:href="<?= BASEURL; ?>/img/bootstrap-icons-1.2.1/bootstrap-icons.svg#plus" />
+                </svg>
+                Tambah
+            </a>
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <div class="input-group-text bg-light border-right-0">
+                        <svg class="bi text-secondary" width="16" height="16" fill="currentColor">
+                            <use xlink:href="<?= BASEURL; ?>/img/bootstrap-icons-1.2.1/bootstrap-icons.svg#search" />
+                        </svg>
+                    </div>
+                </div>
+                <input type="search" class="search form-control border-left-0 bg-light" placeholder="Cari Paket" aria-label="Paket">
+            </div>
+
+        </span>
+
+        <div class="d-flex justify-content-between">
+            <p>Total <strong id="total-paket">0</strong> paket sewa</p>
+            <span>
+                <div class="btn-group mx-3">
+                    <a type="button" class="text-primary dropdown-toggle text-decoration-none" data-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false">
+                        <svg class="bi" width="18" height="18" fill="currentColor">
+                            <use xlink:href="<?= BASEURL; ?>/img/bootstrap-icons-1.2.1/bootstrap-icons.svg#filter" />
+                        </svg>
+                        <p class="d-none d-sm-inline">Urutkan</p>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right anim-fade shadow border-0">
+                        <button class="dropdown-item sort" data-sort="nama" type="button">Nama</button>
+                        <button class="dropdown-item sort" data-sort="harga" type="button">Harga</button>
+                    </div>
+                </div>
+                <a type="button" class="text-primary" id="btn-refresh">
+                    <svg class="bi" width="18" height="18" fill="currentColor">
+                        <use xlink:href="<?= BASEURL; ?>/img/bootstrap-icons-1.2.1/bootstrap-icons.svg#arrow-clockwise" />
+                    </svg>
+                    <p class="d-none d-sm-inline">Refresh</p>
+                </a>
+            </span>
+        </div>
+
+        <div id="list-paket-container">
+
+            <span class="row d-none my-5" id="loading-list">
+                <div class="spinner-border text-primary mx-auto" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </span>
+            <div class="d-none flex-column text-secondary text-center align-items-center my-5 list-paket-empty-message">
+                <svg class="bi my-3" width="36" height="36" fill="currentColor">
+                    <use xlink:href="" />
+                </svg>
+                <p class="h4"></p>
+                <p class="font-weight-normal"></p>
+            </div>
+
+            <div class="list list-paket row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 m-0 px-0">
+
+            </div>
+
+            <div class="white-space" style="height: 20vh;"></div>
+        </div>
+
+        <button class="btn btn-primary position-fixed rounded-pill shadow-sm p-2 d-block d-md-none" style="right: 3vw; bottom: 60px; z-index: 1;" onclick="window.location.href = `${BASEURL}/datapaketsewa/pagetambah`">
+            <svg class="bi" width="36" height="36" fill="currentColor">
+                <use xlink:href="<?= BASEURL; ?>/img/bootstrap-icons-1.2.1/bootstrap-icons.svg#plus" />
+            </svg>
+        </button>
+
+    </main>
+</section>
+
+<div class="modal fade" id="dialogHapus" data-keyboard="false" tabindex="-1" aria-labelledby="hapusTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content shadow-lg">
+            <div class="modal-header bg-transparent border-0">
+                <h5 class="modal-title" id="hapusTitle">Konfirmasi Hapus</h5>
+            </div>
+            <div class="modal-body">
+                <p>Anda yakin ingin menghapus "<span class="selected-data"></span>" dari daftar paket sewa?</p>
+            </div>
+            <div class="modal-footer bg-transparent border-0">
+                <a data-dismiss="modal" class="btn text-danger">Batal</a>
+                <a href="" class="btn btn-danger selected-data">Hapus</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="<?= BASEURL ?>/js/list.min.js"></script>
+<script src="<?= BASEURL ?>/js/paketsewa.min.js"></script>
+<script src="<?= BASEURL ?>/js/formatRupiah.min.js"></script>
---
+++ app/views/datapaketsewa/viewdetail.php
@@ -0,0 +1,67 @@
+<section class="col-md mt-3">
+    <header class="p-2 bg-white d-flex align-items-center">
+        <a href="javascript:history.go(-1)" class="float-left" title="Kembali">
+            <svg class="bi text-primary" width="24" height="24" fill="currentColor">
+                <use xlink:href="<?= BASEURL; ?>/img/bootstrap-icons-1.2.1/bootstrap-icons.svg#arrow-left" />
+            </svg>
+        </a>
+        <!-- <h2 class="text-center mx-4 my-0 text-truncate">Detail</h2> -->
+    </header>
+    <main class="mt-3 mx-2 mx-sm-5">
+        <h1 class="display-4 font-weight-normal"><?= $data['formvalue']['nama_paket'] ?></h1>
+        <p class="harga lead font-weight-normal"><?= $data['formvalue']['harga'] ?></p>
+        <div class="d-flex">
+            <a href="<?= BASEURL ?>/datapaketsewa/details/pageedit/<?= $data['formvalue']['id_paket'] ?>" class="btn btn-lg btn-primary m-1">
+                <svg class="bi mr-2" width="18" height="18" fill="currentColor">
+                    <use xlink:href="<?= BASEURL; ?>/img/bootstrap-icons-1.2.1/bootstrap-icons.svg#pencil-fill" />
+                </svg>
+                Edit
+            </a>
+            <a class="btn btn-lg btn-danger m-1" data-toggle="modal" data-target="#dialogHapus">
+                <svg class="bi mr-2" width="18" height="18" fill="currentColor">
+                    <use xlink:href="<?= BASEURL; ?>/img/bootstrap-icons-1.2.1/bootstrap-icons.svg#trash-fill" />
+                </svg>
+                Hapus
+            </a>
+        </div>
+        <div class="mt-5">
+            <h4>Termasuk dalam paket ini</h4>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 m-0">
+                <?php foreach ($data['formvalue']['id_barang'] as $key => $value) : ?>
+                    <div class="col p-0">
+                        <div class="card mx-0 mx-sm-2 my-2 shadow-sm border-0">
+                            <div class="card-body">
+                                <h4 class="display-4"><?= $data['formvalue']['jumlah_barang'][$key] ?></h4>
+                                <p class="lead font-weight-normal"><?= $data['formvalue']['nama_barang'][$key] ?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach ?>
+            </div>
+        </div>
+    </main>
+    <div style="height: 15vh;"></div>
+</section>
+
+<div class="modal fade" id="dialogHapus" data-keyboard="false" tabindex="-1" aria-labelledby="hapusTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content shadow-lg">
+            <div class="modal-header bg-transparent border-0">
+                <h5 class="modal-title" id="hapusTitle">Konfirmasi Hapus</h5>
+            </div>
+            <div class="modal-body">
+                <p>Anda yakin ingin menghapus "<?= $data['formvalue']['nama_paket'] ?>" dari daftar paket sewa?</p>
+            </div>
+            <div class="modal-footer bg-transparent border-0">
+                <a data-dismiss="modal" class="btn text-danger">Batal</a>
+                <a href="<?= BASEURL ?>/datapaketsewa/hapus/<?= $data['formvalue']['id_paket'] ?>" class="btn btn-danger selected-data">Hapus</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="<?= BASEURL ?>/js/formatRupiah.min.js"></script>
+<script>
+    const txtHarga = document.querySelector("p.harga");
+    txtHarga.textContent = `Rp.${formatRupiah(txtHarga.textContent)}`;
+</script>
