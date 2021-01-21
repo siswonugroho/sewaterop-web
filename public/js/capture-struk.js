c2f4469b673f81f43fd2614a73c9c54d80e5276a
Sovana Siswonugroho <vana.hantoro7@gmail.com>
2021-01-21 07:55:46 +0700

    Gambar struk kini berformat jpg

--- public/js/capture-struk.js
+++ public/js/capture-struk.js
@@ -2,7 +2,7 @@ const downloadStrukBtn = document.querySelector('a#downloadStruk');
 html2canvas(document.querySelector('.gambar-struk'), {
     scrollY: -window.scrollY
 }).then(function (canvas) {
-    var base64image = canvas.toDataURL("image/png");
+    var base64image = canvas.toDataURL("image/jpeg", 0.8);
     downloadStrukBtn.href = base64image;
 });
 // downloadStrukBtn.addEventListener('click', function () {
